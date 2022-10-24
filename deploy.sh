87705327cd603e692d8ea9b20e6655e33c2f5e2d
Lucas Germano <lucas.emanuel.germano@gmail.com>
2022-10-24 14:57:25 -0300

    Fixing postgres volume permission

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,14 @@
-git pull origin main && docker compose build && docker compose up -d
+#!/bin/bash
+
+volume=$(docker volume ls --filter name=pg_data_vital_donation | grep pg_data_vital_donation)
+
+if [ -z "$volume" ]
+then
+    echo "Postgres volume does not exists. Creating it..."
+    docker volume create pg_data_vital_donation
+fi
+
+echo "Updating project"
+git pull origin main
+echo "Rebuilding containers"
+docker compose build && docker compose up -d
